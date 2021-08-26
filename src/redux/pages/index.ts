c7e9f8028146c27465c4bd510e4878b9efaa0fa9
k10wl <72667957+k10wl@users.noreply.github.com>
2021-08-26 11:07:40 +0300

    added pages redux state that stores data from api

---
+++ src/redux/pages/index.ts
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  characters: {},
+  locations: {},
+  episodes: {},
+};
+
+const pages = createSlice({
+  name: "pages",
+  initialState,
+  reducers: {
+    storePages: (state, action) => ({
+      ...state,
+      [action.payload.category]: {
+        ...[action.payload.category],
+        [action.payload.page]: action.payload.pageData,
+      },
+    }),
+  },
+});
+
+export default pages;
+
+export const { storePages } = pages.actions;
