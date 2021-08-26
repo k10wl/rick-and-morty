5699a99fede91081919d8d5453cbbc8e0dd1ba92
k10wl <72667957+k10wl@users.noreply.github.com>
2021-08-26 11:07:40 +0300

    added pages redux state that stores data from api

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,8 +1,10 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import filters from "./filters";
+import pages from "./pages";
 
 const reducer = combineReducers({
   filters: filters.reducer,
+  pages: pages.reducer
 });
 
 export default configureStore({
