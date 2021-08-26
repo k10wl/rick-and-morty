426fcf717a3ba13232286d7f474ea32a29afa796
k10wl <72667957+k10wl@users.noreply.github.com>
2021-08-26 10:33:24 +0300

    added new redux state to manage pages

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,8 +1,10 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import filters from "./filters";
+import apiInfo from "./apiInfo";
 
 const reducer = combineReducers({
   filters: filters.reducer,
+  apiInfo: apiInfo.reducer,
 });
 
 export default configureStore({
