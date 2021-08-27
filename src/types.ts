5adfb8ec3678d8e851055486acad2d8a875531fe
k10wl <72667957+k10wl@users.noreply.github.com>
2021-08-27 19:31:39 +0300

    reworked types

--- src/types.ts
+++ src/types.ts
@@ -10,8 +10,14 @@ export type Character = CommonType & {
   status: string;
   species: string;
   gender: string;
-  origin: object;
-  location: object;
+  origin: {
+    name: string;
+    url: string;
+  };
+  location: {
+    name: string;
+    url: string;
+  };
   image: string;
   episode: string[];
 };
