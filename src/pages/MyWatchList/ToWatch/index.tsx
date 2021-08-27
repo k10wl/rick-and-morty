51df8064a895ddd68eb7cb37ff5e501a22bf1063
k10wl <72667957+k10wl@users.noreply.github.com>
2021-08-27 12:07:47 +0300

    toWatch has completed functionality

--- src/pages/MyWatchList/ToWatch/index.tsx
+++ src/pages/MyWatchList/ToWatch/index.tsx
@@ -2,15 +2,22 @@ import * as React from "react";
 import { Button, Typography } from "@material-ui/core";
 
 type Props = {
+  id: number;
   text: string;
   completed: boolean;
+  // eslint-disable-next-line no-unused-vars
+  handleToggle: (id: number) => void;
+  // eslint-disable-next-line no-unused-vars
+  handleRemove: (id: number) => void;
 };
 
-function ToWatch({ text, completed }: Props) {
+function ToWatch({ id, text, completed, handleToggle, handleRemove }: Props) {
   return (
-    <div className={completed ? "comleted" : undefined}>
+    <div className={completed ? "completed" : undefined}>
       <Typography>{text}</Typography>
-      <Button>Toggle</Button>
+      {completed && <p>completed</p>}
+      <Button onClick={() => handleToggle(id)}>Toggle</Button>
+      <Button onClick={() => handleRemove(id)}>Remove</Button>
     </div>
   );
 }
--- src/pages/MyWatchList/index.tsx
+++ src/pages/MyWatchList/index.tsx
@@ -1,18 +1,83 @@
 import * as React from "react";
 import * as Mui from "@material-ui/core";
+import ToWatch from "./ToWatch";
 
-type Props = {};
+type CommentSubmitType =
+  | React.FormEvent<HTMLFormElement>
+  | React.MouseEvent<HTMLAnchorElement>
+  | React.MouseEvent<HTMLButtonElement>;
+type InputType = React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>;
 
-function MyWatchList(props: Props) {
-  console.log(props);
+type ToWatchType = {
+  id: number;
+  text: string;
+  completed: boolean;
+};
+
+function MyWatchList() {
+  const [input, setInput] = React.useState("");
+  const [toWatch, setToWatch] = React.useState<ToWatchType[]>([]);
+  React.useEffect(() => {
+    if (localStorage.getItem("comments")) {
+      setToWatch(JSON.parse(localStorage.getItem("comments") as string));
+    }
+  }, []);
+  React.useEffect(() => {
+    localStorage.setItem("comments", JSON.stringify(toWatch));
+  }, [toWatch]);
+  function handleInput(e: InputType) {
+    setInput(e.currentTarget.value);
+  }
+  function handleSubmit(e: CommentSubmitType) {
+    e.preventDefault();
+    const empty = input.replace(/ /g, "");
+    if (!empty) {
+      return;
+    }
+    const newId = toWatch.length !== 0 ? toWatch[toWatch.length - 1].id + 1 : 1;
+    const comment = {
+      text: input,
+      completed: false,
+      id: newId,
+    };
+    const value = toWatch.length !== 0 ? [...toWatch, comment] : [comment];
+    setToWatch(value);
+    setInput("");
+  }
+  function handleToggle(id: number) {
+    const toggled = toWatch.map((el) => {
+      if (el.id === id) {
+        return {
+          ...el,
+          completed: !el.completed,
+        };
+      }
+      return el;
+    });
+    setToWatch(toggled);
+  }
+  function handleRemove(id: number) {
+    const removed = toWatch.filter((el) => el.id !== id);
+    setToWatch(removed);
+  }
   return (
-    <>
-      <form>
-        <Mui.TextField label="To watch" />
-        <Mui.Button>Add ToWatch</Mui.Button>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Mui.TextField label="To watch" value={input} onChange={handleInput} />
+        <Mui.Button onClick={handleSubmit}>Add ToWatch</Mui.Button>
       </form>
       <h1>MyWatchList</h1>
-    </>
+      {toWatch.map((el: ToWatchType) => (
+        <ToWatch
+          key={el.id}
+          text={el.text}
+          completed={el.completed}
+          id={el.id}
+          handleToggle={handleToggle}
+          handleRemove={handleRemove}
+        />
+      ))}
+    </div>
   );
 }
 
