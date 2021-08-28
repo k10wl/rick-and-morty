16d80d22cbaf7cf9512b6913e3832d177a80fa94
k10wl <72667957+k10wl@users.noreply.github.com>
2021-08-28 17:34:07 +0300

    styled episodes tab

--- src/pages/Episodes/index.tsx
+++ src/pages/Episodes/index.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
 import * as Mui from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 import EpisodesTable from "./EpisodesTable";
 import { Episode, PageType } from "../../types";
 import FetchData from "../../customHooks/FetchData";
+import useStyles from "../../UI/useStyles";
 
 function Episodes() {
   const [apiData, setApiData] = React.useState<Episode[]>([]);
@@ -10,6 +12,7 @@ function Episodes() {
   const [apiUrl, setApiUrl] = React.useState<string>(defaultUrl);
   const [filter, setFilter] = React.useState("");
   const [page, setPage] = React.useState<PageType>([null, null]);
+  const [forceUpdate, setForceUpdate] = React.useState<boolean>(false);
   const { loaded, data, prev, next } = FetchData(apiUrl);
   React.useEffect(() => {
     if (loaded) {
@@ -25,25 +28,50 @@ function Episodes() {
   function handleFilterInput(e: React.ChangeEvent<HTMLTextAreaElement>) {
     setFilter(e.target.value);
   }
+  function handleSubmit(
+    e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement>
+  ) {
+    if (typeof e.preventDefault === "function") {
+      e.preventDefault();
+    }
+    setForceUpdate(!forceUpdate);
+  }
   React.useEffect(() => {
     const query = filter.replace(/ /g, "") !== "" ? `?name=${filter}` : "";
     setApiUrl(defaultUrl + query);
-  }, [filter]);
+  }, [forceUpdate]);
+  const classes = useStyles();
   return (
-    <div>
-      <h1>Episodes</h1>
-      <Mui.TextField
-        variant="outlined"
-        label="Filter"
-        value={filter}
-        onChange={handleFilterInput}
-      />
+    <Mui.Grid className={classes.tabRoot}>
+      <Mui.Typography variant="h4" align="center">
+        List of Episodes
+      </Mui.Typography>
+      <form onSubmit={handleSubmit}>
+        <Mui.Grid container justifyContent="center" alignItems="center">
+          <Mui.TextField
+            variant="outlined"
+            label="Filter"
+            value={filter}
+            onChange={handleFilterInput}
+            fullWidth
+            InputProps={{
+              endAdornment: (
+                <Mui.Grid>
+                  <Mui.IconButton onClick={handleSubmit}>
+                    <SearchIcon />
+                  </Mui.IconButton>
+                </Mui.Grid>
+              ),
+            }}
+          />
+        </Mui.Grid>
+      </form>
       <EpisodesTable
         array={apiData}
         page={page}
         handleSelect={handlePagination}
       />
-    </div>
+    </Mui.Grid>
   );
 }
 
