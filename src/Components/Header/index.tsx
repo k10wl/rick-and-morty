633ba1ac91c05eb45dfc211bd7d03fc1d3e8efa4
k10wl <72667957+k10wl@users.noreply.github.com>
2021-08-28 19:54:57 +0300

    fixed header selection

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,23 +1,50 @@
 import * as React from "react";
 import * as Mui from "@material-ui/core";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 function Header() {
+  const history = useHistory();
+  const [tabsValue, setTabsValue] = React.useState<string>("/");
+  const [forceUpdate, setForceUpdate] = React.useState<boolean>(false);
+  React.useEffect(() => {
+    setTabsValue(history.location.pathname);
+  }, [forceUpdate]);
   return (
     <Mui.AppBar position="static" style={{ margin: "0" }}>
-      <Mui.Grid md={6}>
-        <Mui.Toolbar variant="dense">
-          <Mui.Tab
-            label="Characters"
-            component={Link}
-            to="/"
-            style={{ width: "50px" }}
-          />
-          <Mui.Tab label="Episodes" component={Link} to="/episodes" />
-          <Mui.Tab label="Locations" component={Link} to="/locations" />
-          <Mui.Tab label="toWatch" component={Link} to="/watchlist" />
-        </Mui.Toolbar>
-      </Mui.Grid>
+      <Mui.Toolbar variant="dense">
+        <Mui.Grid container item sm={12} md justifyContent="center">
+          <Mui.Tabs value={tabsValue}>
+            <Mui.Tab
+              value="/"
+              label="Characters"
+              component={Link}
+              to="/"
+              onClick={() => setForceUpdate(!forceUpdate)}
+            />
+            <Mui.Tab
+              value="/episodes"
+              label="Episodes"
+              component={Link}
+              to="/episodes"
+              onClick={() => setForceUpdate(!forceUpdate)}
+            />
+            <Mui.Tab
+              value="/locations"
+              label="Locations"
+              component={Link}
+              to="/locations"
+              onClick={() => setForceUpdate(!forceUpdate)}
+            />
+            <Mui.Tab
+              value="/watchlist"
+              label="toWatch"
+              component={Link}
+              to="/watchlist"
+              onClick={() => setForceUpdate(!forceUpdate)}
+            />
+          </Mui.Tabs>
+        </Mui.Grid>
+      </Mui.Toolbar>
     </Mui.AppBar>
   );
 }
